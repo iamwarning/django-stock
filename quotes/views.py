64875c391fb2c2563c0f6dff415a714cf7766f9a
Siul Gutierrez [rootCrash] <siul.gomez94@gmail.com>
2019-08-14 18:04:08 -0500

    Connect to the API IEX Cloud

--- quotes/views.py
+++ quotes/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'hello.html', {})
+    # pk_18020125b9404116be195d47eee8f33e
+    import requests
+    import json
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote/latestPrice?token=pk_18020125b9404116be195d47eee8f33e")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as ex:
+        api = "..::: Error :::.."
+
+    return render(request, 'hello.html', {'api': api})
 
 
 def about(request):
