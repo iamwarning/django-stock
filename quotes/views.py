bf66656ca4ff0f520829a3c7521cfaa968ce820c
Siul Gutierrez [rootCrash] <siul.gomez94@gmail.com>
2019-08-23 12:06:18 -0500

    Adding functionality to the search form

--- quotes/views.py
+++ quotes/views.py
@@ -7,14 +7,18 @@ def home(request):
     import requests
     import json
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote/?token=pk_18020125b9404116be195d47eee8f33e")
-    # noinspection PyBroadException
-    try:
-        api = json.loads(api_request.content)
-    except Exception as ex:
-        api = "Error..."
-
-    return render(request, 'hello.html', {'api': api})
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote/?token=pk_18020125b9404116be195d47eee8f33e")
+        # noinspection PyBroadException
+        try:
+            api = json.loads(api_request.content)
+        except Exception as ex:
+            api = "Error..."
+        return render(request, 'hello.html', {'api': api})
+    else:
+        return render(request, 'hello.html', {'ticker': "Enter a Ticker Symbol Above..."})
 
 
 def about(request):
